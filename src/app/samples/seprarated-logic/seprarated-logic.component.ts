8dea957dc712f08372173ac0d3e7066d9b683dc6
Sander Elias <sanderelias+git@gmail.com>
2017-10-27 05:45:47 +0200

    updated depedencies

--- src/app/samples/seprarated-logic/seprarated-logic.component.ts
+++ src/app/samples/seprarated-logic/seprarated-logic.component.ts
@@ -3,13 +3,15 @@ import { PeopleService } from '../separated-logic.service';
 import { Person } from '../../interfaces';
 
 @Component({
-  selector: 'seprarated-logic',
-  templateUrl: './seprarated-logic.component.html',
-  styleUrls: ['./seprarated-logic.component.css']
+    selector: 'seprarated-logic',
+    templateUrl: './seprarated-logic.component.html',
+    styleUrls: ['./seprarated-logic.component.css']
 })
 export class SepraratedLogicComponent {
-  persons$ = this.logic.persons$;
-  selectedPerson: Person;
+    persons$ = this.logic.persons$;
+    selectedPerson: Person;
 
-  constructor(private logic: PeopleService) {}
+    constructor(private logic: PeopleService) {
+        console.log('init');
+    }
 }
