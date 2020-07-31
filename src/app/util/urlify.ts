ad80cc1fc630b7aca1c5140621b9d8182a5d33b5
sanderelias <sanderelias+git@gmail.com>
2020-07-31 14:48:08 +0200

    update for GH pages

--- src/app/util/urlify.ts
+++ src/app/util/urlify.ts
@@ -1,2 +1,7 @@
 
-export const urlify= (url) => `${url.replace('http:','https:')}/?format=json`
+export const urlify= (url) => {
+  let r = `${url.replace('http:','https:')}`
+  if (r.includes('format=json')) {return r}
+  if (r.includes('?')) {return `${r}&format=json`}
+  return `${r}?format=json`
+}
